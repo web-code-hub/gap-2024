d864d9a04f1511de08844029fae3ac0b62640bfe
web-code-hub <992640940@qq.com>
2025-09-23 22:00:24 +0800

    style: Counter with Tailwind 2024

--- vue3-2024/src/components/Counter.vue
+++ vue3-2024/src/components/Counter.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="count++">2024-{{count}}</button>
+    <button @click="count++" class="px-4 py-2  bg-sky-500 text-white rounded hover:bg-sky-600">2024-{{count}}</button>
 </template>
 <script setup lang="ts">
 const count = defineModel<Number>({default:0})
